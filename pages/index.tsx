12df0125d6ff0e1b8b69c5115a0f35600a12332d
giwan <giwan.dev@gmail.com>
2022-07-20 00:03:48 +0900

    🔥 예시 간소화

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,15 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
 import { useOverlay } from "next-overlay";
 
 export default function IndexPage() {
   const { visible, raise, dismiss } = useOverlay();
-  const router = useRouter();
 
   return (
     <div>
-      <Link href="?b=foo&a=bar">
-        <a>쿼리 변경 테스트하기</a>
-      </Link>
-
       <button onClick={raise}>Raise Dialog</button>
       {visible ? (
         <div>
           This is dialog
-          <button
-            onClick={async () => {
-              await dismiss();
-
-              await router.push("?foo=bar");
-            }}
-          >
-            close
-          </button>
+          <button onClick={dismiss}>close</button>
         </div>
       ) : null}
     </div>
